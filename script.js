788c7987bdf5cb8d8d2362c733510bb338b4aa70
Remy <remy@chaoticmewtral.com>
2022-10-17 05:36:24 -0400

    Add to script.js and index.html
    Add variables for die and results to script.js.
    
    Add divs to display dice roll results.

--- script.js
+++ script.js
@@ -1,19 +1,23 @@
 // variables
+let dNum;
+let dResult;
 
 // constants
 const dice = document.querySelectorAll('button');
-
+const dieRolled = document.querySelector('#die-rolled');
+const result = document.querySelector('#result');
 
 // event listeners
 dice.forEach(die => die.addEventListener('click', () => rollTheDice(die.textContent)));
 
 // functions
 function rollTheDice(e) {
-    num = parseInt(e.slice(1));
-    roll = Math.ceil(Math.random() * num);
-    showResult(`${e}: ${roll}`);
+    dNum = e;
+    let num = parseInt(e.slice(1));
+    dResult = Math.ceil(Math.random() * num);
+    showResult();
 }
 
-function showResult(e) {
-    console.log(e);
+function showResult() {
+    console.log(`You rolled a ${dNum} and got ${dResult}`);
 }
