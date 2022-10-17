92996542fb41f845d26d0fd3595ca5e56319b93f
Remy <remy@chaoticmewtral.com>
2022-10-17 05:01:19 -0400

    Add defer to script tag in index.html

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 // variables
 
 // constants
+const dice = document.querySelectorAll('button');
 
 // event listeners
+dice.forEach(die => die.addEventListener('click', () => rollTheDice(die.textContent)));
 
 // functions
+function rollTheDice(e) {
+console.log(e);
+}
